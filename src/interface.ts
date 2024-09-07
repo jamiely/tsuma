75d7e387fdfde377a9ded76c7a47cf2611e78209
Jamie Ly <jamie.ly@gmail.com>
2024-09-07 16:16:09 -0400

    Use click instead of mousedown

--- src/interface.ts
+++ src/interface.ts
@@ -5,7 +5,7 @@ export const createInterface = (game: Game) => (element: HTMLElement) => {
   element.addEventListener("mousemove", (event) => {
     game.launcher.pointTo = {x: event.offsetX, y: event.offsetY};
   });
-  element.addEventListener("mousedown", () => {
+  element.addEventListener("click", () => {
     launchBall(game);
   })
 };
